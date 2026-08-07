3b0a8b477c602621b870b69e45af70f93e2b9c3a
chyopriushy <mmkanggg@gmail.com>
2026-08-07 16:10:32 +0900

    [CHORE]: 초기 Backend 디렉토리 생성

---
+++ src/test/java/org/example/voice/VoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.voice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
